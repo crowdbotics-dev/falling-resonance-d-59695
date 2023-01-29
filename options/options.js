40202a080d824d65ea98bc0d031a37a99f631c4b
Crowdbotics <engineering@crowdbotics.com>
2023-01-29 00:12:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-resonance-d-59695",
+  url: "https://falling-resonance-d-59695.botics.co",
+  api: "https://falling-resonance-d-59695.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
